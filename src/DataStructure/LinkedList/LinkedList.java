0a3ad16f6306f24fdd74ba9a7d196d34e8cf2445
Hemant Kumar <hemantkumarvs94@gmail.com>
2017-11-07 22:06:56 +0530

    Traverse a linked list

--- src/DataStructure/LinkedList/LinkedList.java
+++ src/DataStructure/LinkedList/LinkedList.java
@@ -1,40 +1,42 @@
 package DataStructure.LinkedList;
-
-
-
 /**
  * Created by hemantkumar on 11/7/2017.
  */
 
-// LL with 3 nodes
+// Traverse LinkedList with 3 nodes and print all elements
 
-class LinkedList
-{
+class LinkedList {
     Node head;
 
-    static class Node
-    {
+    static class Node {
         int data;
         Node next;
 
-        Node(int d)
-        {
+        Node(int d) {
             data = d;
-            next=null;
+            next = null;
+        }
+    }
+
+    public void printLinkedList() {
+
+        Node n = head;
+        System.out.println("Linked List is ");
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
         }
     }
 
     public static void main(String[] args) {
-
         LinkedList list1 = new LinkedList();
-
         list1.head = new Node(1);
         Node second = new Node(2);
         Node third = new Node(3);
 
         list1.head.next = second;
         second.next = third;
-
-
+        third.next = null;
+        list1.printLinkedList();
     }
 }
