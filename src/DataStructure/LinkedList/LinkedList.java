9d2f937d51b046b7b9c2a548849dd1d60462c2a3
Hemant <hemantkumarvs94@gmail.com>
2017-11-09 16:38:41 +0530

    rfref

--- src/DataStructure/LinkedList/LinkedList.java
+++ src/DataStructure/LinkedList/LinkedList.java
@@ -1,4 +1,7 @@
 package DataStructure.LinkedList;
+
+import javax.xml.soap.Node;
+
 /**
  * Created by hemantkumar on 11/7/2017.
  */
@@ -6,8 +9,9 @@ package DataStructure.LinkedList;
 // Traverse LinkedList with 3 nodes and print all elements
 
 class LinkedList {
-    Node head;
+ Node head;
 
+//    create Node
     static class Node {
         int data;
         Node next;
@@ -18,25 +22,46 @@ class LinkedList {
         }
     }
 
-    public void printLinkedList() {
-
+// print linkedList
+    private void printlinkedlist() {
         Node n = head;
-        System.out.println("Linked List is ");
         while (n != null) {
             System.out.print(n.data + " ");
             n = n.next;
         }
+        System.out.println("\n");
     }
 
+// method to add node at the beginning O(1)
+    private void insertNodeAtBegin(int data) {
+        Node new_node = new Node(data);
+        new_node.next = head;
+        head = new_node;
+    }
+
+// method to add node at the end
+    private void insertNodeAtEnd(int data)
+    {
+
+    }
+
+
+// method to add node at some node
+    private void insertAfter()
+    {}
+    
+
+
     public static void main(String[] args) {
-        LinkedList list1 = new LinkedList();
-        list1.head = new Node(1);
+        LinkedList object = new LinkedList();
+
+        object.head = new Node(1);
         Node second = new Node(2);
         Node third = new Node(3);
-
-        list1.head.next = second;
+        object.head.next = second;
         second.next = third;
-        third.next = null;
-        list1.printLinkedList();
+        object.printlinkedlist();
+        object.insertNodeAtBegin(5);
+        object.printlinkedlist();
     }
 }
